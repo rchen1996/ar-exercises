36fc4a84b851827759ed1fd8ad1221e1bd19f213
Rebecca Chen <rebecca061996@gmail.com>
2021-03-01 19:35:09 +0000

    complete exercise 7 - demonstrate validation errors by asking user for a store name and creating a store without other attributes

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -6,7 +6,15 @@ require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
 
-puts "Exercise 7"
-puts "----------"
+puts 'Exercise 7'
+puts '----------'
 
 # Your code goes here ...
+
+puts 'Please provide a store name'
+store_name = gets.chomp
+
+new_store = Store.create(name: store_name)
+
+error_messages = new_store.errors.full_messages
+error_messages.each { |error| puts error }
