067e9f660cd433c870881d9aefded34d7971b0da
Rebecca Chen <rebecca061996@gmail.com>
2021-03-01 19:04:39 +0000

    add validation

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,21 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue,
+            numericality: {
+              only_integer: true,
+              greater_than_or_equal_to: 0,
+            }
+
+  def must_carry_mens_or_womens
+    if mens_apparel == false && womens_apparel == false
+      errors.add(
+        :mens_apparel,
+        :womens_apparel,
+        message: "at least one of men's or womens apparel must be true",
+      )
+    end
+  end
+
+  validate must_carry_mens_or_womens
 end
