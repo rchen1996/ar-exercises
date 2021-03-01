a138d38d0c4e2a2df4665f6339a52ef09fa1439a
Rebecca Chen <rebecca061996@gmail.com>
2021-03-01 18:41:17 +0000

    complete exercise 5 - using sum and average to output annual revenues for all stores, chaining of where and count to output number of stores generating M or more

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -4,7 +4,12 @@ require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 
-puts "Exercise 5"
-puts "----------"
+puts 'Exercise 5'
+puts '----------'
 
 # Your code goes here ...
+puts Store.sum(:annual_revenue)
+
+puts Store.average(:annual_revenue)
+
+puts Store.where('annual_revenue > ?', 1_000_000).count
