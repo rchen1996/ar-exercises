ada87e90ed746c54062496e02c7fdb189586095c
Rebecca Chen <rebecca061996@gmail.com>
2021-03-01 21:15:06 +0000

    add a new employee and check to see if the password set callback is working

--- exercises/exercise_8.rb
+++ exercises/exercise_8.rb
@@ -6,3 +6,13 @@ require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
 require_relative './exercise_7'
+
+@store2.employees.create(
+  first_name: 'Ryan',
+  last_name: 'Nguyen',
+  hourly_rate: 60,
+)
+
+@employees = Employee.where(first_name: 'Ryan')
+
+@employees.each { |employee| puts "#{employee.password}" }
