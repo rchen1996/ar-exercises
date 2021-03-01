6912996bfe41ce0bdf2b7a1457c1cf13304b5888
Rebecca Chen <rebecca061996@gmail.com>
2021-03-01 18:52:12 +0000

    complete exercise 6 - create employees for stores 1 and 2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -5,7 +5,36 @@ require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
 
-puts "Exercise 6"
-puts "----------"
+puts 'Exercise 6'
+puts '----------'
 
 # Your code goes here ...
+@store1.employees.create(
+  first_name: 'Khurram',
+  last_name: 'Virani',
+  hourly_rate: 60,
+)
+
+@store1.employees.create(
+  first_name: 'Rebecca',
+  last_name: 'Chen',
+  hourly_rate: 90,
+)
+
+@store1.employees.create(
+  first_name: 'Evelyn',
+  last_name: 'Yoa',
+  hourly_rate: 70,
+)
+
+@store2.employees.create(
+  first_name: 'Brady',
+  last_name: 'Blair',
+  hourly_rate: 90,
+)
+
+@store2.employees.create(
+  first_name: 'May',
+  last_name: 'Cheong',
+  hourly_rate: 30,
+)
